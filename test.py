247af5e896ec9747200272b448895e1fc0871983
Triet Vo <vominhtriet02@gmail.com>
2021-11-15 15:08:10 -0500

    Finish register, login and integrate with database

--- test.py
+++ test.py
@@ -3,72 +3,66 @@ import pymysql
 import hashlib
 
 app = Flask("Triet")
-"""
+
 conn = pymysql.connect(host = 'localhost',
                        user = 'root',
                        password = 'trietrie',
-                       db='CS3083',
+                       db='cs3083',
                        charset = 'utf8mb4',
                        cursorclass = pymysql.cursors.DictCursor)
 
 
 def md5(s):
-    return hashlib.md5(s).hexdigest()
-"""
+    return hashlib.md5(s.encode()).hexdigest()
+
 @app.route("/")
 def hello_world():
     return "<p> Hello, World! </p>"
 
 @app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name = None):
-    return render_template('hello.html', name = name)
+def hello():
+    return render_template('hello.html', name = request.args.get('name'))
 
 @app.route('/login/', methods = ['GET','POST'])
 def login():
     error = None
     if request.method == 'POST':
-        """
-        username = escape(request.form['username'])
-        password = md5(escape(request.form['password']))
+        username = request.form['username']
+        print(username)
+        password = md5(request.form['password'])
         cursor = conn.cursor()
-        query = 'SELECT * FROM user WHERE username = %s AND password = %s'
+        query = 'SELECT * FROM Users WHERE username = %s AND password = %s'
         cursor.execute(query, (username, password))
 
         data = cursor.fetchone()
         cursor.close()
-        """
-        # if not (request.form['username'] == 'admin' and request.form['password'] == 'admin') and not data:
-        if not (request.form['username'] == 'admin' and request.form['password'] == 'admin'):
-            error = 'Invalid Credentials...'
+        if not (request.form['username'] == 'admin' and request.form['password'] == 'admin') and not data:            error = 'Invalid Credentials...'
         else:
-            return redirect(url_for('hello'))
+            return redirect(url_for('hello', name = username))
     return render_template('login.html', error = error)
 
-"""
 @app.route('/register/', methods = ['GET','POST'])
 def register():
-    error = None
+    status = None
     if request.method == 'POST':
         
         username = request.form['username']
         password = md5(escape(request.form['password']))
         cursor = conn.cursor()
-        query = 'SELECT * FROM user WHERE username = %s'
+        query = 'SELECT * FROM Users WHERE username = %s'
         cursor.execute(query, (username))
 
         data = cursor.fetchone()
 
         if data:
-            error = 'This user already exists'
+            status = 'This user already exists'
         else:
-            ins = 'INSERT INTO user VALUES(%s, %s)'
+            ins = 'INSERT INTO Users VALUES(%s, %s)'
             cursor.execute(ins, (username, password))
             conn.commit()
-            error = 'Registered =)'
+            status = 'Registered =)'
         cursor.close()
-    return render_template('login.html', error = error)
-"""
+    return render_template('register.html', status = status)
 if __name__ == "__main__":
     app.run('127.0.0.1', 5000, debug = True)
 
