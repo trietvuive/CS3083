5f5fdd1c5429af2aa4f4f4544f158816c60e2d28
Triet Vo <vominhtriet02@gmail.com>
2021-11-24 15:20:46 -0500

    Add html for searching flights

--- flask_route/root/hello_world.py
+++ flask_route/root/hello_world.py
@@ -1,6 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from route import home
 
-@home.route("/")
+@home.route('/')
 def hello_world():
     return "<p> Hello, World! </p>"
+@home.route('/flights')
+def search_flight():
+    return render_template('search_flight.html')
