0aae01bd6b3f7423c3ad5ddab06c231d81cc423b
Mateo <mc7212@nyu.edu>
2021-12-01 16:04:34 -0500

    Added spaces to queries

--- settings.py
+++ settings.py
@@ -92,22 +92,22 @@ staff_log_veri_query = 'SELECT * FROM AirlineStaff WHERE username = %s AND passw
 # Use Case 4
 # View MY Flights
 # View All Future Flights
-cust_future_flights = 'SELECT *' \
-                      'FROM Flight' \
-                      'WHERE (airline_name, flight_num) IN' \
-                      '(SELECT airline, flight_num' \
-                      'FROM Ticket, Purchases' \
+cust_future_flights = 'SELECT * ' \
+                      'FROM Flight ' \
+                      'WHERE (airline_name, flight_num) IN ' \
+                      '(SELECT airline, flight_num ' \
+                      'FROM Ticket, Purchases ' \
                       'WHERE id = t_id AND cust_email = %s ' \
                       'AND depart_datetime > CURRENT_TIMESTAMP)'
 
 # View All Flights in a Range of Dates
-cust_range_flights = 'SELECT *' \
-                     'FROM Flight' \
-                     'WHERE (airline_name, flight_num) IN' \
-                     '(SELECT airline, flight_num' \
-                     'FROM Ticket, Purchases' \
-                     'WHERE id = t_id AND cust_email = %s' \
-                     'AND CAST(depart_datetime AS date) >= %s' \
+cust_range_flights = 'SELECT * ' \
+                     'FROM Flight ' \
+                     'WHERE (airline_name, flight_num) IN ' \
+                     '(SELECT airline, flight_num ' \
+                     'FROM Ticket, Purchases ' \
+                     'WHERE id = t_id AND cust_email = %s ' \
+                     'AND CAST(depart_datetime AS date) >= %s ' \
                      'AND CAST(depart_datetime AS date) <= %s)'
 
 # Use Case 5
@@ -118,7 +118,7 @@ cust_range_flights = 'SELECT *' \
 # Figure out sold price
 
 # Purchase the Ticket
-cust_purchase_ticket = 'INSERT INTO Ticket VALUES(UUID(), sold_price, %s, %s, %s, %s, %s, %s, %s)'
+cust_purchase_ticket = 'INSERT INTO Ticket VALUES(UUID_TO_BIN(UUID()), sold_price, %s, %s, %s, %s, %s, %s, %s)'
 
 # Record Purchase (Need to have same ticket id as UUID())
 cust_record_purchase = 'INSERT INTO Purchases VALUES(%s, %s, CURRENT_TIMESTAMP)'
@@ -130,35 +130,35 @@ cust_record_purchase = 'INSERT INTO Purchases VALUES(%s, %s, CURRENT_TIMESTAMP)'
 # cust_email, flight_airline, flight_num, flight_depart_datetime
 cust_ins_takes = 'INSERT INTO Takes VALUES(%s, %s, %s, %s, NULL, NULL)'
 
-cust_rate_comment = 'UPDATE Takes' \
-                    'SET comment = %s, rating = %i' \
-                    'WHERE cust_email = %s AND flight_airline = %s' \
+cust_rate_comment = 'UPDATE Takes ' \
+                    'SET comment = %s, rating = %i ' \
+                    'WHERE cust_email = %s AND flight_airline = %s ' \
                     'AND flight_num = %s AND flight_depart_datetime = %s'
 
 # Use Case 8
 # Total Spent in Past Year
-cust_spent_year = 'SELECT SUM(sold_price)' \
-                  'FROM Purchases, Ticket' \
-                  'WHERE cust_email = %s AND t_ID = ID' \
-                  'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 YEAR)' \
+cust_spent_year = 'SELECT SUM(sold_price) ' \
+                  'FROM Purchases, Ticket ' \
+                  'WHERE cust_email = %s AND t_ID = ID ' \
+                  'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 YEAR) ' \
                   'AND CAST(date_time AS date) <= CURDATE()'
 
 # Total Spent in Last 6 Months by Month (DEFAULT)
-cust_spent_monthly_sixmonths = 'SELECT YEAR(date_time), MONTHNAME(date_time), SUM(sold_price)' \
-                     'FROM Purchases, Ticket' \
-                     'WHERE cust_email = %s AND t_ID = ID' \
-                     'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -6 MONTH)' \
-                     'AND CAST(date_time AS date) <= CURDATE()' \
-                     'GROUP BY YEAR(date_time), MONTHNAME(date_time)' \
+cust_spent_monthly_sixmonths = 'SELECT YEAR(date_time), MONTHNAME(date_time), SUM(sold_price) ' \
+                     'FROM Purchases, Ticket ' \
+                     'WHERE cust_email = %s AND t_ID = ID ' \
+                     'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -6 MONTH) ' \
+                     'AND CAST(date_time AS date) <= CURDATE() ' \
+                     'GROUP BY YEAR(date_time), MONTHNAME(date_time) ' \
                      'ORDER BY YEAR(date_time), MONTHNAME(date_time)'
 
 # Total Spent in Range of Dates by Month
-cust_spent_monthly_range = 'SELECT YEAR(date_time), MONTHNAME(date_time), SUM(sold_price)' \
-                           'FROM Purchases, Ticket' \
-                           'WHERE cust_email = %s AND t_ID = ID' \
+cust_spent_monthly_range = 'SELECT YEAR(date_time), MONTHNAME(date_time), SUM(sold_price) ' \
+                           'FROM Purchases, Ticket ' \
+                           'WHERE cust_email = %s AND t_ID = ID ' \
                            'AND CAST(date_time AS date) >= %s ' \
-                           'AND CAST(date_time AS date) <= %s' \
-                           'GROUP BY YEAR(date_time), MONTHNAME(date_time)' \
+                           'AND CAST(date_time AS date) <= %s ' \
+                           'GROUP BY YEAR(date_time), MONTHNAME(date_time) ' \
                            'ORDER BY YEAR(date_time), MONTHNAME(date_time)'
 
 # -------------------------------------------- Staff Use Cases -----------------------------------------------
@@ -166,30 +166,30 @@ cust_spent_monthly_range = 'SELECT YEAR(date_time), MONTHNAME(date_time), SUM(so
 
 # Use Case 4
 # Show All Future Flights Operated by Staff's Airline in Next 30 Days (DEFAULT)
-staff_show_flights_30days = 'SELECT * FROM Flight' \
-                            'WHERE CAST(depart_datetime AS date) >= CURDATE()' \
+staff_show_flights_30days = 'SELECT * FROM Flight ' \
+                            'WHERE CAST(depart_datetime AS date) >= CURDATE() ' \
                             'AND CAST(depart_datetime AS date) <= DATE_ADD(CURDATE(), INTERVAL 30 DAY) ' \
-                            'AND airline_name IN' \
-                            '(SELECT airline' \
-                            'FROM AirlineStaff' \
+                            'AND airline_name IN ' \
+                            '(SELECT airline ' \
+                            'FROM AirlineStaff ' \
                             'WHERE username = %s)'
 
 # Show All Flights Operated by Staff's Airline in Range of Dates
-staff_show_flights_range = 'SELECT *' \
-                           'FROM Flight' \
-                           'WHERE CAST(depart_datetime AS date) >= %s' \
-                           'AND CAST(depart_datetime AS date) <= %s' \
-                           'AND airline_name IN' \
-                           '(SELECT airline' \
-                           'FROM AirlineStaff' \
+staff_show_flights_range = 'SELECT * ' \
+                           'FROM Flight ' \
+                           'WHERE CAST(depart_datetime AS date) >= %s ' \
+                           'AND CAST(depart_datetime AS date) <= %s ' \
+                           'AND airline_name IN ' \
+                           '(SELECT airline ' \
+                           'FROM AirlineStaff ' \
                            'WHERE username = %s)'
 
 # Show All Customers of a Flight
-staff_show_customers = 'SELECT email, name' \
-                       'FROM Customer, Purchases' \
-                       'WHERE email = cust_email AND t_id IN' \
-                       '(SELECT id' \
-                       'FROM Ticket' \
+staff_show_customers = 'SELECT email, name ' \
+                       'FROM Customer, Purchases ' \
+                       'WHERE email = cust_email AND t_id IN ' \
+                       '(SELECT id ' \
+                       'FROM Ticket ' \
                        'WHERE airline = %s AND flight_num = %s AND depart_datetime = %s)'
 
 # Use Case 5
@@ -221,90 +221,94 @@ staff_ins_airport = 'INSERT INTO Airport VALUES(%s, %s, %s)'
 
 # Use Case 9
 # Average Rating for a Flight
-staff_show_avg_rating = 'SELECT flight_airline, flight_num, flight_depart_datetime, AVG(rating) AS Average Rating' \
-                        'FROM Takes' \
-                        'WHERE flight_airline = %s AND flight_num = %s' \
-                        'AND flight_depart_datetime = %s' \
+staff_show_avg_rating = 'SELECT flight_airline, flight_num, flight_depart_datetime, AVG(rating) AS Average Rating ' \
+                        'FROM Takes ' \
+                        'WHERE flight_airline = %s AND flight_num = %s ' \
+                        'AND flight_depart_datetime = %s ' \
                         'GROUP BY flight_airline, flight_num, flight_depart_datetime'
 
 # Ratings and Comments for a Flight
-staff_show_ratings_comments = 'SELECT cust_email, comment, rating' \
-                              'FROM Takes' \
-                              'WHERE flight_airline = %s AND flight_num = %s' \
+staff_show_ratings_comments = 'SELECT cust_email, comment, rating ' \
+                              'FROM Takes ' \
+                              'WHERE flight_airline IN ' \
+                              '(SELECT airline ' \
+                              'FROM AirlineStaff ' \
+                              'WHERE username = %s) ' \
+                              'AND flight_num = %s ' \
                               'AND flight_depart_datetime = %s'
 
 # Use Case 10
 # View Most Frequent Customer Within Last Year
-staff_show_freq_customer = 'SELECT cust_email, COUNT(cust_email)' \
-                           'FROM Takes' \
-                           'WHERE flight_airline IN' \
-                           '(SELECT airline' \
-                           'FROM AirlineStaff' \
-                           'WHERE username = %s)' \
-                           'AND CAST(flight_depart_datetime AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 YEAR)' \
-                           'AND CAST(flight_depart_datetime AS date) <= CURDATE()' \
-                           'AND cust_email NOT IN' \
-                           '(SELECT s.cust_email' \
-                           'FROM Takes AS s, TAKES AS t' \
+staff_show_freq_customer = 'SELECT cust_email, COUNT(cust_email) ' \
+                           'FROM Takes ' \
+                           'WHERE flight_airline IN ' \
+                           '(SELECT airline ' \
+                           'FROM AirlineStaff ' \
+                           'WHERE username = %s) ' \
+                           'AND CAST(flight_depart_datetime AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 YEAR) ' \
+                           'AND CAST(flight_depart_datetime AS date) <= CURDATE() ' \
+                           'AND cust_email NOT IN ' \
+                           '(SELECT s.cust_email ' \
+                           'FROM Takes AS s, TAKES AS t ' \
                            'WHERE COUNT(s.cust_email) < COUNT(t.cust_email))'
 
 # View All Flights a Customer Has Taken on Airline
-staff_show_customer_flights = 'SELECT flight_num, flight_depart_datetime' \
-                              'FROM Takes' \
-                              'WHERE cust_email = %s' \
-                              'AND flight_airline IN' \
-                              '(SELECT airline' \
-                              'FROM AirlineStaff' \
+staff_show_customer_flights = 'SELECT flight_num, flight_depart_datetime ' \
+                              'FROM Takes ' \
+                              'WHERE cust_email = %s ' \
+                              'AND flight_airline IN ' \
+                              '(SELECT airline ' \
+                              'FROM AirlineStaff ' \
                               'WHERE username = %s)'
 
 # Use Case 11
 # View Reports
 # Total Amount of Tickets Sold in Range of Dates
-staff_total_tickets_sold = 'SELECT COUNT(t_id)' \
-                           'FROM Ticket, Purchases' \
-                           'WHERE id = t_id' \
-                           'AND CAST(date_time AS date) >= %s' \
-                           'AND CAST(date_time AS date) <= %s' \
-                           'AND airline IN' \
-                           '(SELECT airline' \
-                           'FROM AirlineStaff' \
+staff_total_tickets_sold = 'SELECT COUNT(t_id) ' \
+                           'FROM Ticket, Purchases ' \
+                           'WHERE id = t_id ' \
+                           'AND CAST(date_time AS date) >= %s ' \
+                           'AND CAST(date_time AS date) <= %s ' \
+                           'AND airline IN ' \
+                           '(SELECT airline ' \
+                           'FROM AirlineStaff ' \
                            'WHERE username = %s)'
 
 # Monthwise Tickets Sold in Range of Dates
-staff_monthwise_tickets_sold = 'SELECT YEAR(date_time), MONTHNAME(date_time), COUNT(t_id)' \
-                               'FROM Ticket, Purchases' \
-                               'WHERE id = t_id' \
-                               'AND CAST(date_time AS date) >= %s' \
-                               'AND CAST(date_time AS date) <= %s' \
-                               'AND airline IN' \
-                               '(SELECT airline' \
-                               'FROM AirlineStaff' \
-                               'WHERE username = %s)' \
-                               'GROUP BY YEAR(date_time), MONTHNAME(date_time)' \
+staff_monthwise_tickets_sold = 'SELECT YEAR(date_time), MONTHNAME(date_time), COUNT(t_id) ' \
+                               'FROM Ticket, Purchases ' \
+                               'WHERE id = t_id ' \
+                               'AND CAST(date_time AS date) >= %s ' \
+                               'AND CAST(date_time AS date) <= %s ' \
+                               'AND airline IN ' \
+                               '(SELECT airline ' \
+                               'FROM AirlineStaff ' \
+                               'WHERE username = %s) ' \
+                               'GROUP BY YEAR(date_time), MONTHNAME(date_time) ' \
                                'ORDER BY YEAR(date_time), MONTHNAME(date_time)'
 
 # Use Case 12
 # View Earnings
 # Revenue of Ticket Sales in Last Month
-staff_revenue_month = 'SELECT SUM(sold_price)' \
-                      'FROM Ticket, Purchases' \
-                      'WHERE t_id = id' \
-                      'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 MONTH)' \
-                      'AND CAST(date_time AS date) <= CURDATE()' \
-                      'AND airline IN' \
-                      '(SELECT airline' \
-                      'FROM AirlineStaff' \
+staff_revenue_month = 'SELECT SUM(sold_price) ' \
+                      'FROM Ticket, Purchases ' \
+                      'WHERE t_id = id ' \
+                      'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 MONTH) ' \
+                      'AND CAST(date_time AS date) <= CURDATE() ' \
+                      'AND airline IN ' \
+                      '(SELECT airline ' \
+                      'FROM AirlineStaff ' \
                       'WHERE username = %s)'
 
 # Revenue of Ticket Sales in Last Year
-staff_revenue_year = 'SELECT SUM(sold_price)' \
-                     'FROM Ticket, Purchases' \
-                     'WHERE t_id = id' \
-                     'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 YEAR)' \
-                     'AND CAST(date_time AS date) <= CURDATE()' \
+staff_revenue_year = 'SELECT SUM(sold_price) ' \
+                     'FROM Ticket, Purchases ' \
+                     'WHERE t_id = id ' \
+                     'AND CAST(date_time AS date) >= DATE_ADD(CURDATE(), INTERVAL -1 YEAR) ' \
+                     'AND CAST(date_time AS date) <= CURDATE() ' \
                      'AND airline IN ' \
-                     '(SELECT airline' \
-                     'FROM AirlineStaff' \
+                     '(SELECT airline ' \
+                     'FROM AirlineStaff ' \
                      'WHERE username = %s)'
 
 # Use Case 13 -- NOT FINISHED
